b8851707d3c9b845cd30af499cd90e50c73b18b8
BROOKE <LILRODGERS58@GMAIL.COM>
2022-08-08 16:45:26 -0400

    homework

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,11 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO workouts (workout_name,workout_desc,expected_target) VALUES ('workout1','workout desc','arms'),
+('workout2','workout desc','legs'),
+('workout3','workout desc','back'),
+('workout4','workout desc','cardio'),
+('workout5','workout desc','total body');
+
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -15,12 +15,39 @@ CREATE TABLE workouts (
     workout_id SERIAL,
     workout_name varchar(50) NOT NULL UNIQUE,
     workout_desc varchar(500) NOT NULL,
-    weight varchar(20),
-    expected_rep_set varchar(100) NOT NULL,
-    expected_time int,
     expected_target varchar(100),
     CONSTRAINT valid_target CHECK(expected_target IN ('arms','legs','abs','back','cardio','total body')),
     CONSTRAINT PK_workout PRIMARY KEY (workout_id)
 );
+
+CREATE TABLE beginner_workouts (
+    beginner_id SERIAL,
+    workout_id SERIAL,
+    weight varchar(20),
+    expected_rep_set varchar(100) NOT NULL,
+    expected_time int,
+    CONSTRAINT FK_workout FOREIGN KEY (workout_id) REFERENCES workouts(workout_id),
+    CONSTRAINT PK_beginner PRIMARY KEY (beginner_id)
+    );
+
+CREATE TABLE intermediate_workouts (
+        intermediate_id SERIAL,
+        workout_id SERIAL,
+        weight varchar(20),
+        expected_rep_set varchar(100) NOT NULL,
+        expected_time int,
+        CONSTRAINT FK_workout FOREIGN KEY (workout_id) REFERENCES workouts(workout_id),
+        CONSTRAINT PK_intermediate PRIMARY KEY (intermediate_id)
+        );
+
+ CREATE TABLE extreme_workouts (
+         extreme_id SERIAL,
+         workout_id SERIAL,
+         weight varchar(20),
+         expected_rep_set varchar(100) NOT NULL,
+         expected_time int,
+         CONSTRAINT FK_workout FOREIGN KEY (workout_id) REFERENCES workouts(workout_id),
+         CONSTRAINT PK_extreme PRIMARY KEY (extreme_id)
+          );
 
 COMMIT TRANSACTION;
