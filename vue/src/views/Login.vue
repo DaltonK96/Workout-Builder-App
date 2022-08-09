4133c4e821651347e98be6f7877adaeace4fa428
Cesar Andino <inarikaze@gmail.com>
2022-08-09 12:16:13 -0400

    Styles

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+      <h1 class="Orange-Workout-Builder" id="app-title">Orange Workout Builder</h1>
+      <h1 class="h3 mb-3 font-weight-normal" id="psi">Please Sign In</h1>
       <div
         class="alert alert-danger"
         role="alert"
@@ -12,7 +13,7 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
+      <label id="login-username-label" for="username" class="sr-only">Username</label>
       <input
         type="text"
         id="username"
@@ -74,3 +75,4 @@ export default {
   }
 };
 </script>
+
