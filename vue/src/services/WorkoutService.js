e1eca4757d58357b3334d7e381ebacabe32a411a
BROOKE <LILRODGERS58@GMAIL.COM>
2022-08-12 10:57:22 -0400

    capstone

--- vue/src/services/WorkoutService.js
+++ vue/src/services/WorkoutService.js
@@ -4,5 +4,16 @@ export default {
 
   addExercise(user) {
     return axios.post('/workouts', user)
+  },
+
+  editExercise(user,level){
+    if(level=== "beginner"){
+      return axios.post('/workouts/beginner',user)
+    } else if( level === "intermediate"){
+      return axios.post('/workouts/intermediate',user)
+    } else{
+      return axios.post('/workouts/extreme',user)
+    }
+
+    }
   }
-}
