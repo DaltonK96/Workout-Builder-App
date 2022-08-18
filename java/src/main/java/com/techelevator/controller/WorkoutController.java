33e0c3ffe0b4504e5c172fb425b65b73581daaa0
Jessica Atkinson <jesslynn1007@gmail.com>
2022-08-18 13:25:33 -0400

    commit

--- java/src/main/java/com/techelevator/controller/WorkoutController.java
+++ java/src/main/java/com/techelevator/controller/WorkoutController.java
@@ -142,9 +142,17 @@ public class WorkoutController
     }
 
     @RequestMapping(path = "/randomWorkouts", method = RequestMethod.POST)
-    public List<Generator> createRandomWorkout(Principal user, @RequestBody GeneratorDTO generatorDTO)
+    public int createRandomWorkout(Principal user, @RequestBody GeneratorDTO generatorDTO)
     {
-        return workoutGeneratorDao.createRandomWorkouts(generatorDTO.getTarget(), generatorDTO.getTime(), userDao.findIdByUsername(user.getName()), generatorDTO.getDifficulty());
+        List <Generator> newGeneratedWorkouts =  workoutGeneratorDao.createRandomWorkouts(generatorDTO.getTarget(), generatorDTO.getTime(), userDao.findIdByUsername(user.getName()), generatorDTO.getDifficulty());
+        int generatedId = 0;
+
+        for (Generator generator: newGeneratedWorkouts)
+        {
+            generatedId = generator.getGeneratedId();
+            break;
+        }
+        return generatedId;
     }
 
     @RequestMapping(path = "/workouts/history", method = RequestMethod.GET)
